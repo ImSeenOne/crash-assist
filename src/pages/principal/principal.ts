9edfdd2e1939ac85f9ab2827c40b4308763937d9
Chronos_Timebreaker <chronostimebreaker@gmail.com>
2018-08-02 01:47:32 -0500

    bugs del git

--- src/pages/principal/principal.ts
+++ src/pages/principal/principal.ts
@@ -1,9 +1,6 @@
 import { Component } from '@angular/core';
-<<<<<<< HEAD
 import { IonicPage, NavController, MenuController } from 'ionic-angular';
-=======
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
->>>>>>> 04fce7f93fae48bbcc8d34f7b4e06f8359099fa8
 
 import {Pagina2Page,EmergenciasPage} from "../index.pages";
 
@@ -13,27 +10,22 @@ import {Pagina2Page,EmergenciasPage} from "../index.pages";
   templateUrl: 'principal.html',
 })
 export class PrincipalPage {
-<<<<<<< HEAD
   pagina2:any = Pagina2Page;
   constructor(public navCtrl: NavController,
               private menuCtrl: MenuController) {}
-=======
+
   // emer:any = EmergenciasPage;
   // pagina2:any = Pagina2Page;
 
   constructor(public navCtrl: NavController,  public navParams: NavParams) {}
->>>>>>> 04fce7f93fae48bbcc8d34f7b4e06f8359099fa8
 
   navegarPagina(){
     this.navCtrl.push(Pagina2Page);
   }
-<<<<<<< HEAD
   mostrarMenu(){
     this.menuCtrl.toggle();
-=======
   navegarNumeros(){
     this.navCtrl.push(EmergenciasPage);
->>>>>>> 04fce7f93fae48bbcc8d34f7b4e06f8359099fa8
   }
 
 }
