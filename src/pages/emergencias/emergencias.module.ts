acbf081dd5c5fb4ffa1a2c261e5484c615d35e87
Chronos_Timebreaker <chronostimebreaker@gmail.com>
2018-08-01 21:47:24 -0500

    Chronos-Changes used Emergencias

--- src/pages/emergencias/emergencias.module.ts
+++ src/pages/emergencias/emergencias.module.ts
@@ -12,28 +12,7 @@ import { EmergenciasPage } from './emergencias';
 })
 export class EmergenciasPageModule {
 
-  numeros:any[] = [
-    {
-      nombre:"Num. Nacional",
-      numero:"911"
-    },
-    {
-      nombre:"Informacion General",
-      numero:"040"
-    },
-    {
-      nombre:"Radio Patrullas",
-      numero:"Choque con otro vehiculo"
-    },
-    {
-      nombre:"Bomberos",
-      numero:"Fallo mecanico"
-    },
-    {
-      nombre:"Denuncia anonima",
-      numero:"numero pro"
-    }
-  ];
+
 
 
 }
--- src/pages/emergencias/emergencias.ts
+++ src/pages/emergencias/emergencias.ts
@@ -14,6 +14,28 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'emergencias.html',
 })
 export class EmergenciasPage {
+  numeros:any[] = [
+    {
+      nombre:"Num. Nacional",
+      numero:"911"
+    },
+    {
+      nombre:"Informacion General",
+      numero:"040"
+    },
+    {
+      nombre:"Radio Patrullas",
+      numero:"Choque con otro vehiculo"
+    },
+    {
+      nombre:"Bomberos",
+      numero:"Fallo mecanico"
+    },
+    {
+      nombre:"Denuncia anonima",
+      numero:"numero pro"
+    }
+  ];
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
