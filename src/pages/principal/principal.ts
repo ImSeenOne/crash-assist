d57a78d4ea273a5643cfdf954ca05a54c273286a
ImSeenOne <cristianramirezdev@hotmail.com>
2018-08-03 03:40:08 -0500

    Inject AngularFire and Bind it to List

--- src/pages/principal/principal.ts
+++ src/pages/principal/principal.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, MenuController } from 'ionic-angular';
 import {Pagina2Page,EmergenciasPage} from "../index.pages";
+import { AngularFireDatabase } from 'angularfire2/database';
+import { Observable } from 'rxjs/Observable';
 
 @IonicPage()
 @Component({
@@ -8,9 +10,13 @@ import {Pagina2Page,EmergenciasPage} from "../index.pages";
   templateUrl: 'principal.html',
 })
 export class PrincipalPage {
+  items: Observable<any[]>;
   pagina2:any = Pagina2Page;
   constructor(public navCtrl: NavController,
-              private menuCtrl: MenuController) {}
+              private menuCtrl: MenuController,
+              afDB: AngularFireDatabase) {
+                this.items = afDB.list('cuisines').valueChanges();
+              }
 
   // emer:any = EmergenciasPage;
   // pagina2:any = Pagina2Page;
